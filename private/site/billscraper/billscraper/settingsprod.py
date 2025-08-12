ed6e17bcf5af536a0c78b0003eddb0f5854d03a1
zmarquis13 <zenomarquis13@gmail.com>
2025-08-12 03:14:36 +0000

    changed url on allowed hosts

--- private/site/billscraper/billscraper/settingsprod.py
+++ private/site/billscraper/billscraper/settingsprod.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['aipolicydatabase.com', '146.190.121.58']
+ALLOWED_HOSTS = ['aipolicydb.com', '146.190.121.58']
 
 CSRF_TRUSTED_ORIGINS = [
         'https://aipolicydatabase.com',
